e82b483ea7eb6f21c2c1de6f1e67ad7288cc11a5
James <justjamescreative@outlook.com>
2025-04-20 18:08:15 -0400

    Created a runnable class - Assignment8Application
    I instantiated the Assignment8 class - "assignment".
    Fetched the data using the getData() method.
    Filtered and printed the results for numbers 1 - 10 with the filterNumbers() method.

---
+++ src/main/java/com/coderscampus/assignment/Assignment8Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignment;
+
+import java.util.List;
+
+public class Assignment8Application {
+    public static void main(String[] args) {
+
+        Assignment8 assignment = new Assignment8();
+        List<Integer> numbers = assignment.getData();
+        assignment.filterNumbers(numbers);
+    }
+}
+
