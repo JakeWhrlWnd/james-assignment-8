e93181771b9494e424156528a44faeb1fc66bbaa
James <justjamescreative@outlook.com>
2025-04-28 12:59:55 -0400

    Changed the Executor shutdown to a helper method

--- src/main/java/com/coderscampus/assignment/Assignment8.java
+++ src/main/java/com/coderscampus/assignment/Assignment8.java
@@ -78,22 +78,26 @@ public class Assignment8 {
                 allNumbers.addAll(task.join());
             }
         } finally {
-            pool.shutdown();
-            try {
-                if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
-                    pool.shutdownNow();
-                    if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
-                        System.err.println("Pool did not terminate");
-                    }
-                }
-            } catch (InterruptedException e) {
-                pool.shutdownNow();
-                Thread.currentThread().interrupt();
-            }
+            shutdownExecutor(pool);
         }
 
 
         return allNumbers;
+    }
+
+    private void shutdownExecutor(ExecutorService pool) {
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Pool did not terminate");
+                }
+            }
+        } catch (InterruptedException e) {
+            pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 
     public void filterNumbers(List<Integer> numbers) {
