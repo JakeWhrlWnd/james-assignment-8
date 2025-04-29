36ed4cbfaf0b3cb7ba4d7dc19bf3ed0b1f723768
James <justjamescreative@outlook.com>
2025-04-29 09:00:48 -0400

    Added logic to the main file

--- src/main/java/com/coderscampus/assignment/Assignment8Application.java
+++ src/main/java/com/coderscampus/assignment/Assignment8Application.java
@@ -1,13 +1,14 @@
 package com.coderscampus.assignment;
 
+import com.coderscampus.assignment.service.NumberProcessingService;
+
 import java.util.List;
 
 public class Assignment8Application {
     public static void main(String[] args) {
 
-        Assignment8 assignment = new Assignment8();
-        List<Integer> numbers = assignment.getData();
-        assignment.filterNumbers(numbers);
+        NumberProcessingService service = new NumberProcessingService();
+        service.processNumbers();
     }
 }
 
